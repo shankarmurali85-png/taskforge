c70185d490eda7db3ebbe1dc84580f41c04e91af
shankarmurali85-png <shankarmurali85@gmail.com>
2026-06-09 15:59:30 +0530

    Update admin.py

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Project,
+    Bid,
+    Delivery,
+    Review,
+    EmailVerificationToken,
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Bid)
+admin.site.register(Delivery)
+admin.site.register(Review)
+admin.site.register(EmailVerificationToken)
