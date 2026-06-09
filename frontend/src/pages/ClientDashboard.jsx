58b38c7f829d11b72ff988ce1a37ba07449cc51d
shankarmurali85-png <shankarmurali85@gmail.com>
2026-06-09 19:44:55 +0530

    Update ClientDashboard.jsx

--- frontend/src/pages/ClientDashboard.jsx
+++ frontend/src/pages/ClientDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE = "https://taskforge-698p.onrender.com/api/";
+const API_BASE = "https://taskforge-698p.onrender.com/api";
 
 function ClientDashboard() {
   const [user, setUser] = useState(null);
